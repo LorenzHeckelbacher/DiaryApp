e8a4e6daae80b1ff5c7064d4f7af94e874cf78ab
Daniel <danielbauer1860@gmail.com>
2019-08-12 15:47:50 +0200

    Bottom Navigationbar

---
+++ app/src/main/java/com/example/dailythougths/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.dailythougths;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupBottomNavigationBar();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
+    }
+
+    private void setupBottomNavigationBar() {
+        bottomNav = findViewById(R.id.bottom_navigation);
+
+        BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_calendar:
+                        selectedFragment = new CalendarFragment();
+                        break;
+                    case R.id.nav_mood_stats:
+                        selectedFragment = new MoodFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        };
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+    }
+
+
+}
